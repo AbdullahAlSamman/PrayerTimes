72a15c5e3c5f31d25a1b1b3f553fae9c60ecf75c
Abdullah Al Samman <abdullah.alsamman@immowelt.de>
2022-12-08 18:08:15 +0100

    -Add hilt dependency -Upgrade Gradle version -Change traget os to 33 API -Update gms dependency

--- app/src/main/java/com/gals/prayertimes/App.kt
+++ app/src/main/java/com/gals/prayertimes/App.kt
@@ -4,10 +4,12 @@ import android.app.Application
 import com.gals.prayertimes.model.Prayer
 import com.gals.prayertimes.repository.db.AppDB
 import com.gals.prayertimes.utils.UtilsManager
+import dagger.hilt.android.HiltAndroidApp
 
 typealias DomainPrayer = Prayer
 typealias EntityPrayer = com.gals.prayertimes.repository.db.entities.Prayer
 
+@HiltAndroidApp
 class App : Application() {
     lateinit var tools: UtilsManager
     lateinit var prayer: Prayer
