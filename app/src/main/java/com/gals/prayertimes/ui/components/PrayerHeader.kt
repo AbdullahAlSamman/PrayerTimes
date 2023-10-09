a21fa7b950ed4bc80523a8acd0639a750ca843ce
Abdullah Al Samman <abdullah.alsaman@gmail.com>
2023-10-09 15:55:50 +0200

    release 1.0.4 un disable settings

--- app/src/main/java/com/gals/prayertimes/ui/components/PrayerHeader.kt
+++ app/src/main/java/com/gals/prayertimes/ui/components/PrayerHeader.kt
@@ -1,9 +1,13 @@
 package com.gals.prayertimes.ui.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.painterResource
@@ -33,16 +37,16 @@ fun PrayerHeader(
             contentDescription = stringResource(id = R.string.content_descriptor_background),
             contentScale = ContentScale.FillWidth
         )
-        /*        Icon(
-                    modifier = Modifier
-                        .layoutId("settingsButton")
-                        .clickable {
-                            onSettingsClicked()
-                        },
-                    tint = (if (isSystemInDarkTheme()) Color.White else Color.Black),
-                    painter = painterResource(id = R.drawable.icon_rounded_setting),
-                    contentDescription = stringResource(id = R.string.content_descriptor_settings_icon)
-                )*/
+        Icon(
+            modifier = Modifier
+                .layoutId("settingsButton")
+                .clickable {
+                    onSettingsClicked()
+                },
+            tint = (if (isSystemInDarkTheme()) Color.White else Color.Black),
+            painter = painterResource(id = R.drawable.icon_rounded_setting),
+            contentDescription = stringResource(id = R.string.content_descriptor_settings_icon)
+        )
         ConstraintLayout(
             modifier = Modifier.layoutId("prayerRemainingText"),
             constraintSet = PrayerHeaderConstraint.row
