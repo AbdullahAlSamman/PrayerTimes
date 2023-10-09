e071041778862d107b030bd4476206b6f5a6732e
Abdullah Al Samman <abdullah.alsaman@gmail.com>
2023-10-09 15:39:01 +0200

    convert prayer headers to dynamic sized text

--- app/src/main/java/com/gals/prayertimes/ui/components/PrayerHeader.kt
+++ app/src/main/java/com/gals/prayertimes/ui/components/PrayerHeader.kt
@@ -1,31 +1,28 @@
 package com.gals.prayertimes.ui.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.gals.prayertimes.R
 import com.gals.prayertimes.model.UiNextPrayer
 import com.gals.prayertimes.ui.components.constraint.PrayerHeaderConstraint
 import com.gals.prayertimes.ui.theme.LightTextStyle
+import com.gals.prayertimes.utils.DynamicSizedText
 import com.gals.prayertimes.utils.nonScaledSp
 
 @Composable
 fun PrayerHeader(
     config: UiNextPrayer,
     onSettingsClicked: () -> Unit,
-    textStyle: TextStyle = LightTextStyle.copy(fontSize = 20.nonScaledSp )
+    textStyle: TextStyle = LightTextStyle.copy(fontSize = 20.nonScaledSp)
 ) { /*TODO: consider tablet sizes*/
     ConstraintLayout(constraintSet = PrayerHeaderConstraint.header) {
         Image(
@@ -36,35 +33,38 @@ fun PrayerHeader(
             contentDescription = stringResource(id = R.string.content_descriptor_background),
             contentScale = ContentScale.FillWidth
         )
-/*        Icon(
-            modifier = Modifier
-                .layoutId("settingsButton")
-                .clickable {
-                    onSettingsClicked()
-                },
-            tint = (if (isSystemInDarkTheme()) Color.White else Color.Black),
-            painter = painterResource(id = R.drawable.icon_rounded_setting),
-            contentDescription = stringResource(id = R.string.content_descriptor_settings_icon)
-        )*/
+        /*        Icon(
+                    modifier = Modifier
+                        .layoutId("settingsButton")
+                        .clickable {
+                            onSettingsClicked()
+                        },
+                    tint = (if (isSystemInDarkTheme()) Color.White else Color.Black),
+                    painter = painterResource(id = R.drawable.icon_rounded_setting),
+                    contentDescription = stringResource(id = R.string.content_descriptor_settings_icon)
+                )*/
         ConstraintLayout(
             modifier = Modifier.layoutId("prayerRemainingText"),
             constraintSet = PrayerHeaderConstraint.row
         ) {
-            Text(
+            DynamicSizedText(
                 modifier = Modifier.layoutId("prayerRemainingString"),
                 text = config.nextPrayerBanner,
-                style = textStyle
+                style = textStyle,
+                textAlign = TextAlign.Center
             )
-            Text(
+            DynamicSizedText(
                 modifier = Modifier.layoutId("prayerNameString"),
                 text = config.nextPrayerName,
-                style = textStyle
+                style = textStyle,
+                textAlign = TextAlign.Center
             )
         }
-        Text(
+        DynamicSizedText(
             modifier = Modifier.layoutId("prayerRemainingTime"),
             text = config.nextPrayerTime,
-            style = textStyle.copy(fontSize = 22.nonScaledSp)
+            style = textStyle.copy(fontSize = 22.nonScaledSp),
+            textAlign = TextAlign.Center
         )
     }
 }
