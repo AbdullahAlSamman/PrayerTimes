574de847bcf3c4ed4d710e15f4c43546a6dbb498
Abdullah Al Samman <abdullah.alsaman@gmail.com>
2024-04-22 19:40:25 +0200

    Upgrade all libs to latest versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    val kotlinVersion = "1.8.10"
+    val kotlinVersion = "1.9.23"
 
     repositories {
         mavenCentral()
@@ -9,7 +9,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.3.2")
-        classpath("com.google.gms:google-services:4.4.0")
+        classpath("com.google.gms:google-services:4.4.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("org.jetbrains.kotlin:kotlin-android-extensions:$kotlinVersion")
         // NOTE: Do not place your application dependencies here; they belong
@@ -18,9 +18,9 @@ buildscript {
 }
 
 plugins {
-    id("com.google.dagger.hilt.android") version "2.47" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
-    id("org.jetbrains.kotlin.kapt") version "1.8.10" apply false
+    id("com.google.dagger.hilt.android") version "2.49" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("org.jetbrains.kotlin.kapt") version "1.9.23" apply false
 }
 
 allprojects {
