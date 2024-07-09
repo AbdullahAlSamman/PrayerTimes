fcfbb86a716b4dd7858a0a71e32ac684d70b5371
AbdullahAlSamman <abdullah.alsaman@gmail.com>
2024-07-09 09:21:35 +0200

    test cleanup

--- app/src/test/java/com/gals/prayertimes/viewmodel/MainViewModelTest.kt
+++ app/src/test/java/com/gals/prayertimes/viewmodel/MainViewModelTest.kt
@@ -1,8 +1,6 @@
 package com.gals.prayertimes.viewmodel
 
 import app.cash.turbine.test
-import com.gals.prayertimes.model.ConnectivityException
-import com.gals.prayertimes.model.NetworkException
 import com.gals.prayertimes.model.UiState
 import com.gals.prayertimes.repository.Repository
 import com.gals.prayertimes.repository.local.entities.PrayerEntity
@@ -10,11 +8,9 @@ import com.gals.prayertimes.utils.Formatter
 import com.gals.prayertimes.utils.PrayerCalculation
 import com.gals.prayertimes.utils.ResourceProvider
 import com.gals.prayertimes.utils.TestScreenUpdater
-import com.gals.prayertimes.utils.toPrayer
 import com.gals.prayertimes.viewmodel.utils.TestDispatcherRule
 import com.gals.prayertimes.viewmodel.utils.anyString
 import com.gals.prayertimes.viewmodel.utils.dateString
-import com.gals.prayertimes.viewmodel.utils.errorMessage
 import com.gals.prayertimes.viewmodel.utils.testNextPrayerConfig
 import com.gals.prayertimes.viewmodel.utils.testPrayer
 import com.gals.prayertimes.viewmodel.utils.testPrayerEntity
@@ -25,11 +21,9 @@ import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
-import net.bytebuddy.matcher.ElementMatchers.any
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.jupiter.api.assertThrows
 
 class MainViewModelTest {
 
