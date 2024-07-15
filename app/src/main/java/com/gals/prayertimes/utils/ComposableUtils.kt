be95481c080603c4cf174eee39dcbe5b66582a94
AbdullahAlSamman <abdullah.alsaman@gmail.com>
2024-07-15 12:55:21 +0200

    add composable screen helpers

---
+++ app/src/main/java/com/gals/prayertimes/utils/ComposableUtils.kt
@@ -0,0 +1,41 @@
+package com.gals.prayertimes.utils
+
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.res.Configuration
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
+
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+@Composable
+fun isTablet(): Boolean = when (windowWidthSizeClass()) {
+    WindowWidthSizeClass.Medium, WindowWidthSizeClass.Expanded -> true
+    else -> false
+}
+
+@ExperimentalMaterial3WindowSizeClassApi
+@Composable
+private fun windowWidthSizeClass(): WindowWidthSizeClass? {
+    return LocalContext.current.getActivity()?.let { activity ->
+        val windowSizeClass = calculateWindowSizeClass(activity)
+        windowSizeClass.widthSizeClass
+    }
+}
+
+@Composable
+fun isLandscape(): Boolean = when (LocalConfiguration.current.orientation) {
+    Configuration.ORIENTATION_LANDSCAPE -> true
+    else -> false
+}
+
+private fun Context.getActivity(): Activity? = when (this) {
+    is Activity -> this
+    is ContextWrapper -> baseContext.getActivity()
+    else -> null
+}
+
