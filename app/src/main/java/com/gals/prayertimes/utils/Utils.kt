a907e2ddcf7e8d951d84ec3d9e06389ef73033c9
AbdullahAlSamman <abdullah.alsaman@gmail.com>
2024-09-08 12:50:58 +0200

    T17: adjust mappers

--- app/src/main/java/com/gals/prayertimes/utils/Utils.kt
+++ app/src/main/java/com/gals/prayertimes/utils/Utils.kt
@@ -1,5 +1,12 @@
 package com.gals.prayertimes.utils
 
 import android.os.Build
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.sp
+
+val Int.nonScaledSp
+    @Composable
+    get() = (this / LocalDensity.current.fontScale).sp
 
 val checkAPILevelForAlarms: Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
