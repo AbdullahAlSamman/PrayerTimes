2b62ef0308e10c67e5761bc71d85ed58b13835ad
AbdullahAlSamman <abdullah.alsaman@gmail.com>
2025-07-05 12:21:53 +0200

    T34: update kotlin version 2.2.0 + SDK to 36

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val kotlinVersion = "1.9.23"
+    val kotlinVersion = "2.2.0"
 
     repositories {
         mavenCentral()
@@ -8,7 +8,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.11.0")
-        classpath("com.google.gms:google-services:4.4.2")
+        classpath("com.google.gms:google-services:4.4.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("org.jetbrains.kotlin:kotlin-android-extensions:$kotlinVersion")
         // NOTE: Do not place your application dependencies here; they belong
@@ -17,9 +17,10 @@ buildscript {
 }
 
 plugins {
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
-    id("org.jetbrains.kotlin.kapt") version "1.9.23" apply false
+    id("com.google.dagger.hilt.android") version "2.56.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
+    id("org.jetbrains.kotlin.kapt") version "2.2.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.0" apply false
 }
 
 allprojects {
