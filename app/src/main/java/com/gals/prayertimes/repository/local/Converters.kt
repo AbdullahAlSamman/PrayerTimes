ad6596c3c99d5cae934145de1945efdc16970d0f
AbdullahAlSamman <abdullah.alsaman@gmail.com>
2025-07-13 15:01:55 +0200

    T17: code cleanup

--- app/src/main/java/com/gals/prayertimes/repository/local/Converters.kt
+++ app/src/main/java/com/gals/prayertimes/repository/local/Converters.kt
@@ -2,21 +2,8 @@ package com.gals.prayertimes.repository.local
 
 import androidx.room.TypeConverter
 import com.gals.prayertimes.model.NotificationType
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
 
 object Converters {
-    @TypeConverter
-    fun fromTimestamp(value: Long?): Date? = if (value == null) null else Date(value)
-
-    @TypeConverter
-    fun stringToDate(date: String): Date? =
-        SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US).parse(date)
-
-    @TypeConverter
-    fun stringToBoolean(bool: String?): Boolean = bool.toBoolean()
-
     @TypeConverter
     fun fromNotificationType(value: NotificationType): String = value.name
 
