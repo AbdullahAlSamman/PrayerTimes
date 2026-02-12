8088927fbaf23057dfe5b6c22552e1839f1dcb55
Abdullah Al Samman <abdullah.alsaman@gmail.com>
2026-02-12 10:05:28 +0100

    T113: fix ConsentModule.kt imports for release variant

--- app/src/release/java/com/gals/prayertimes/di/ConsentModule.kt
+++ app/src/release/java/com/gals/prayertimes/di/ConsentModule.kt
@@ -1,7 +1,7 @@
 package com.gals.prayertimes.di
 
+import com.gals.prayertimes.ads.ConsentManager
 import com.gals.prayertimes.ads.consent.AdsConsentManager
-import com.gals.prayertimes.ads.manager.ConsentManager
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -15,6 +15,6 @@ abstract class ConsentModule {
     @Binds
     @Singleton
     abstract fun bindConsentManager(
-        consentManagerImpl: AdsConsentManager
+        adsConsentManager: AdsConsentManager
     ): ConsentManager
 }
