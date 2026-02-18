85845eb4106c4229f721e691a1bf5142b33d4af6
Abdullah Al Samman <abdullah.alsaman@gmail.com>
2026-02-18 01:55:09 +0100

    release: add missing release consent manager class

---
+++ app/src/release/java/com/gals/prayertimes/ads/consent/AdsConsentManager.kt
@@ -0,0 +1,79 @@
+package com.gals.prayertimes.ads.consent
+
+import android.app.Activity
+import com.gals.prayertimes.ads.ConsentManager
+import com.google.android.ump.ConsentInformation
+import com.google.android.ump.ConsentRequestParameters
+import com.google.android.ump.FormError
+import com.google.android.ump.UserMessagingPlatform
+import kotlinx.coroutines.suspendCancellableCoroutine
+import javax.inject.Inject
+import javax.inject.Singleton
+import kotlin.coroutines.resume
+
+@Singleton
+class AdsConsentManager @Inject constructor(
+    val consentInformation: ConsentInformation
+) : ConsentManager {
+
+    override val canRequestAds: Boolean
+        get() = consentInformation.canRequestAds()
+
+    override val isPrivacyOptionsRequired: Boolean
+        get() = consentInformation.privacyOptionsRequirementStatus == ConsentInformation.PrivacyOptionsRequirementStatus.REQUIRED
+
+    override suspend fun gatherConsent(
+        activity: Activity
+    ): FormError? {
+        return suspendCancellableCoroutine { continuation ->
+            requestConsent(
+                activity = activity,
+                onSuccess = {
+                    if (continuation.isActive) {
+                        continuation.resume(null)
+                    }
+                }, onFailure = { formError ->
+                    if (continuation.isActive) {
+                        continuation.resume(formError)
+                    }
+                })
+        }
+    }
+
+    override fun showPrivacyOptionsForm(
+        activity: Activity,
+        onDismiss: (FormError?) -> Unit
+    ) {
+        UserMessagingPlatform.showPrivacyOptionsForm(
+            activity,
+            onDismiss
+        )
+    }
+
+    private fun requestConsent(
+        activity: Activity,
+        onSuccess: () -> Unit,
+        onFailure: (FormError?) -> Unit
+    ) {
+
+        val params = ConsentRequestParameters.Builder()
+            .build()
+
+        consentInformation.requestConsentInfoUpdate(
+            activity,
+            params,
+            {
+                UserMessagingPlatform.loadAndShowConsentFormIfRequired(activity) { formError ->
+                    if (formError != null) {
+                        onFailure(formError)
+                    } else {
+                        onSuccess()
+                    }
+                }
+            },
+            { requestConsentError ->
+                onFailure(requestConsentError)
+            }
+        )
+    }
+}
