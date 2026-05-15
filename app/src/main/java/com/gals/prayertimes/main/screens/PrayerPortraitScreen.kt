119d294207abab50f6f7aca16e1de711e4f20539
Abdullah Al Samman <abdullah.alsaman@gmail.com>
2026-05-15 12:59:36 +0200

    T165: Add missing previews and update existing once.

--- app/src/main/java/com/gals/prayertimes/main/screens/PrayerPortraitScreen.kt
+++ app/src/main/java/com/gals/prayertimes/main/screens/PrayerPortraitScreen.kt
@@ -1,5 +1,7 @@
 package com.gals.prayertimes.main.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
@@ -7,19 +9,27 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.gals.prayertimes.R
+import com.gals.prayertimes.common.UiPrayerName
 import com.gals.prayertimes.main.model.UiDate
 import com.gals.prayertimes.main.model.UiNextPrayer
 import com.gals.prayertimes.main.model.UiPrayerEntry
 import com.gals.prayertimes.main.screens.details.PrayerCell
 import com.gals.prayertimes.main.screens.details.PrayerDateBar
 import com.gals.prayertimes.main.screens.details.PrayerHeader
+import com.gals.prayertimes.utils.PrayerContentPreviewTheme
+import com.gals.prayertimes.utils.PrayerPreviewPortrait
 import com.google.common.collect.ImmutableList
 
 @Composable
@@ -65,5 +75,48 @@ internal fun PrayerPortraitScreen(
         BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
             adBanner(maxHeight)
         }
+    }
+}
+
+@PrayerPreviewPortrait
+@Composable
+private fun PrayerScreenContentPreview() {
+    PrayerContentPreviewTheme {
+        PrayerPortraitScreen(
+            prayers = ImmutableList.of(
+                UiPrayerEntry(UiPrayerName.FAJER, "05:00"),
+                UiPrayerEntry(UiPrayerName.SUNRISE, "06:30"),
+                UiPrayerEntry(UiPrayerName.DUHR, "12:00"),
+                UiPrayerEntry(UiPrayerName.ASR, "15:30"),
+                UiPrayerEntry(UiPrayerName.MAGHRIB, "18:00"),
+                UiPrayerEntry(UiPrayerName.ISHA, "19:30"),
+            ),
+            uiNextPrayer = UiNextPrayer(
+                backgroundImage = R.drawable.background_day,
+                nextPrayerName = "المغرب",
+                nextPrayerTime = "06:00",
+                nextPrayerBanner = "02:30:00"
+            ),
+            uiDate = UiDate(
+                dayName = "الاثنين",
+                moonDate = "15 رمضان 1445",
+                sunDate = "25 مارس 2024"
+            ),
+            adBanner = {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp)
+                        .background(MaterialTheme.colorScheme.inversePrimary)
+                ) {
+                    Text(
+                        modifier = Modifier.align(Alignment.Center),
+                        text = "Advertisement",
+                        style = MaterialTheme.typography.titleLarge,
+                        color = MaterialTheme.colorScheme.surface
+                    )
+                }
+            }
+        )
     }
 }
