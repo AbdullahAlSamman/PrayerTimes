a2795fe94b6a282ebfafc0e5ab42dca6a56a3ce9
Abdullah Al Samman <abdullah.alsaman@gmail.com>
2026-05-17 21:22:09 +0200

    Release dump code and name version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ configure<ApplicationExtension> {
         applicationId = "com.gals.prayertimes"
         minSdk = 26
         targetSdk = 37
-        versionName = "1.3.0"
-        versionCode = 34
+        versionName = "1.3.1"
+        versionCode = 35
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         buildConfigField("String", "BASE_URL", "\"http://prayersapi.scienceontheweb.net/\"")
