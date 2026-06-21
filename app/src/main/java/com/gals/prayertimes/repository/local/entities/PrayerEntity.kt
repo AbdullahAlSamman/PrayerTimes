b76bacee2ec8ed19a496afd3a8377c3234ca27ee
Abdullah Al Samman <abdullah.alsaman@gmail.com>
2026-06-22 00:41:05 +0200

    T180: attempt to fix the migration issue from firebase

--- app/src/main/java/com/gals/prayertimes/repository/local/entities/PrayerEntity.kt
+++ app/src/main/java/com/gals/prayertimes/repository/local/entities/PrayerEntity.kt
@@ -1,17 +1,28 @@
 package com.gals.prayertimes.repository.local.entities
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "prayers")
 data class PrayerEntity(
-    @PrimaryKey val objectId: String = "",
+    @PrimaryKey
+    @ColumnInfo(defaultValue = "")
+    val objectId: String = "",
+    @ColumnInfo(defaultValue = "")
     val sDate: String = "",
+    @ColumnInfo(defaultValue = "")
     val mDate: String = "",
+    @ColumnInfo(defaultValue = "")
     var fajer: String = "",
+    @ColumnInfo(defaultValue = "")
     var sunrise: String = "",
+    @ColumnInfo(defaultValue = "")
     var duhr: String = "",
+    @ColumnInfo(defaultValue = "")
     var asr: String = "",
+    @ColumnInfo(defaultValue = "")
     var maghrib: String = "",
+    @ColumnInfo(defaultValue = "")
     var isha: String = ""
 )
